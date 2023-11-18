f1c3847e21fb74027fce8449f33558f2473f4dc3
Constanza Maria Reis da Silva Mariano <constanza.mariano@usp.br>
2023-11-18 20:37:18 -0300

    Create Django Form

--- theportfolio/urls.py
+++ theportfolio/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from . import views
+from .views import HomeView, PostsListView, SinglePostDetailView, EditPostView, CreatePostView, RemovePostView
 
 urlpatterns = [
-    path('', views.home, name="home"),
-    path('posts/', views.post_list, name="post_list"),
-    path('posts/<int:pk>/', views.post_single, name="post_single"),
+    path('', HomeView.as_view(), name="home"),
+    path('posts/', PostsListView.as_view(), name="post_list"),
+    path('posts/<int:pk>/', SinglePostDetailView.as_view(), name="post_single"),
+    path('create_post/', CreatePostView.as_view(), name="create_post"),
+    path('editar/posts', EditPostView.as_view(), name="edit_post"),
+    path('excluir/<int:pk>', RemovePostView.as_view(), name="remove_post"),
 ]
--- theportfolio/views.py
+++ theportfolio/views.py
@@ -1,13 +1,58 @@
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
 
-def home(request):
-    return render(request, 'home.html', {})
+# def home(request):
+#     return render(request, 'home.html', {})
 
-def post_list(request):
-    posts = Post.objects.all()
-    return render(request, 'post_list.html', {'posts': posts})
+# def post_list(request):
+#     posts = Post.objects.all()
+#     return render(request, 'post_list.html', {'posts': posts})
 
-def post_single(request, pk):
-    single_post = get_object_or_404(Post, id= int(pk) )
-    return render(request, 'post_single.html', {'single_post': single_post})
+# def post_single(request, pk):
+#     single_post = get_object_or_404(Post, id= int(pk) )
+#     return render(request, 'post_single.html', {'single_post': single_post})
+
+class HomeView(ListView):
+    model = None
+    template_name = 'home.html'
+
+    def get_queryset(self):
+        return Post.objects.order_by('-post_date')[:5] #I want to show the latest 5 posts. 
+
+class PostsListView(ListView):
+    model = Post
+    template_name = 'post_list.html'
+    context_object_name = 'posts'
+
+class SinglePostDetailView(DetailView):
+    model = Post
+    template_name = 'post_single.html'
+    context_object_name = 'post_single'
+
+    #Responde com 404 caso seja requisitado um post inexistente.
+    def get_object(self, queryset=None):
+        try:
+            return super().get_object(queryset)
+        except Http404:
+            raise Http404("O item solicitado não existe.")
+
+class CreatePostView(CreateView):
+    model = Post
+    template_name = 'create_post.html'
+    fields = '__all__'
+
+class EditPostView(UpdateView):
+    model = Post
+    template_name = 'edit_post.html'
+    fields = ['title', 'content', 'post_date']
+
+class RemovePostView(DeleteView):
+    model = Post
+    template_name = 'remove_post.html'
+
+    #Implementar uma página de confirmação
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['confirmar_exclusao'] = True  # Adiciona uma variável de confirmação ao contexto
+        return context
