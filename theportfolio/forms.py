3b20a4410a1b663dd51fa300d4529fd2dc531b4f
Constanza Maria Reis da Silva Mariano <constanza.mariano@usp.br>
2023-11-18 21:00:19 -0300

    Create Django Form

--- theportfolio/forms.py
+++ theportfolio/forms.py
@@ -5,4 +5,12 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('title', 'author', 'content', 'post_date')
+
+        widget = {
+            'title': form.TextInput(attrs={'class': 'form-control'}), 
+            'author': form.TextInput(attrs={'class': 'form-control'}), 
+            'content': form.TextInput(attrs={'class': 'form-control'}), 
+            'post_date': form.TextInput(attrs={'class': 'form-control'}), 
+        }
+
     
